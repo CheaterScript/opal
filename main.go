d66b29d3c67a130ad5f8ce49d64fec62bcd0c87f
AI&I <iluozan@126.com>
2019-06-14 20:30:36 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
-	// "io"
-	"log"
-	// "net/http"
-	// "fmt"
+	"fmt"
 	"github.com/luozan/opal/websocket"
+	"io"
+	"net/http"
 )
 
 func main() {
-	// http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-	// 	header := req.Header
-	// 	log.Println(req)
-	// 	log.Println(header)
-	// 	io.WriteString(w, "xxxxxxxxxxxxxx")
-	// })
-	// http.ListenAndServe(":80", nil)
-	log.Println(websocket.SecWebsocketAccept("dGhlIHNhbXBsZSBub25jZQ=="))
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		header := req.Header
+		if _, ok := header["Sec-WebSocket-Key"]; !ok {
+			fmt.Println(ok)
+			io.WriteString(w, "xxxxxxxxxxxxxx")
+			w.WriteHeader(200)
+			return
+		}
+		key := websocket.SecWebsocketAccept(header["Sec-WebSocket-Key"][0])
+		w.Header().Set("Upgrade", "websocket")
+		w.Header().Set("Connection", "Upgrade")
+		w.Header().Set("Sec-WebSocket-Accept", key)
+		w.WriteHeader(101)
+	})
+	http.ListenAndServe(":80", nil)
 }
