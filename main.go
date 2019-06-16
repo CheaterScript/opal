07d5e73d574b339514ca264f2e0964b93fe4f78e
AI&I <iluozan@126.com>
2019-06-16 22:03:08 +0800

    Update main.go

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		header := req.Header
-		if _, ok := header["Sec-WebSocket-Key"]; !ok {
-			fmt.Println(ok)
+		fmt.Println(header)
+		if _, ok := header["Sec-Websocket-Key"]; !ok {
 			io.WriteString(w, "xxxxxxxxxxxxxx")
 			w.WriteHeader(200)
 			return
 		}
-		key := websocket.SecWebsocketAccept(header["Sec-WebSocket-Key"][0])
+		key := websocket.SecWebsocketAccept(header["Sec-Websocket-Key"][0])
 		w.Header().Set("Upgrade", "websocket")
 		w.Header().Set("Connection", "Upgrade")
 		w.Header().Set("Sec-WebSocket-Accept", key)
