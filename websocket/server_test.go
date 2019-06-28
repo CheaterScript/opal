91048d0343059ef46f5a836644b2f6a5085810b9
AI&I <iluozan@126.com>
2019-06-28 18:16:45 +0800

    Update server_test.go

--- websocket/server_test.go
+++ websocket/server_test.go
@@ -1,8 +1,27 @@
 package websocket
 
 import (
+	"bufio"
+	"net"
+	"net/http"
 	"testing"
 )
+
+type fakeHijacker struct {
+}
+
+func (hijacker *fakeHijacker) Header() http.Header{
+	return nil
+}
+func (hijacker *fakeHijacker) Write([]byte) (int, error){
+	return 0, nil
+}
+func (hijacker *fakeHijacker) WriteHeader(statusCode int) {
+	return
+}
+func (hijacker *fakeHijacker) Hijack()(net.Conn, *bufio.ReadWriter, error){
+	return  nil,nil,nil
+}
 
 func TestSecWebsocketAccept(t *testing.T) {
 	key := "dGhlIHNhbXBsZSBub25jZQ=="
@@ -12,9 +31,29 @@ func TestSecWebsocketAccept(t *testing.T) {
 	{
 		result := SecWebsocketAccept(key)
 		if result != expect {
-			t.Fatalf("返回值不正确, 期望:%s, 结果:%s", expect, result)
-		} else {
-			t.Log("正确")
+			t.Fatalf("Wrong result, expectation:%s, result:%s", expect, result)
+		}
+	}
+}
+
+func TestHijack(t *testing.T) {
+	t.Log("Hijacked the connection of HTTP.")
+	{
+		hijacker := &fakeHijacker{}
+		_, _, err := Hijack(hijacker)
+		if err != nil {
+			t.Fatalf("unknown error.")
+		}
+	}
+}
+
+func TestGenResponseHeader(t *testing.T) {
+	key := "dGhlIHNhbXBsZSBub25jZQ=="
+	t.Log("Given the string of response header.")
+	{
+		header := GenResponseHeader(key)
+		if len(header) <= 0{
+			t.Fatal("The length of header less than zero.")
 		}
 	}
 }
